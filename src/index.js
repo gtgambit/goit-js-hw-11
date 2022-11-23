07525add2e49f3e4c30889184c1c63ccf444e24a
gtgambit <gtgambit@gmail.com>
2022-11-23 12:23:04 +0200

    Working beta

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import { ApiService } from './js/ApiPixabay';
+import createPhotoCards from './templates/photocard.hbs';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 350,
+});
+
+const apiService = new ApiService();
+
+const formEl = document.querySelector('#search-form');
+const divEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+async function onFormSubmit(event) {
+  event.preventDefault();
+  apiService.page = 1;
+  apiService.searchQuery = event.target.elements.searchQuery.value.trim();
+  if (!apiService.searchQuery) {
+    Notiflix.Notify.failure('Что-то ввести бы');
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  } else {
+    try {
+      divEl.innerHTML = '';
+      const { data } = await apiService.fetchPhotos();
+      renderPhoto(data);
+      lightbox.refresh();
+    } catch {
+      error => console.log(error);
+    }
+  }
+}
+
+function renderPhoto(data) {
+  if (data.totalHits === 0) {
+    Notiflix.Notify.failure('Таких фото у нас нет');
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  } else if (data.hits < apiService.per_page) {
+    divEl.innerHTML = createPhotoCards(data.hits);
+    loadMoreBtn.classList.add('is-hidden');
+    lightbox.refresh();
+    return;
+  } else if (
+    apiService.page === Math.ceil(data.totalHits / apiService.per_page)
+  ) {
+    divEl.innerHTML = createPhotoCards(data.hits);
+    lightbox.refresh();
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+  Notiflix.Notify.success(`Мы поскребли по сусекам и нашли для вас пару фото`);
+  divEl.innerHTML = createPhotoCards(data.hits);
+  lightbox.refresh();
+  loadMoreBtn.classList.remove('is-hidden');
+  loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+}
+
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+async function onLoadMoreBtnClick(event) {
+  apiService.page += 1;
+  const { data } = await apiService.fetchPhotos();
+  divEl.insertAdjacentHTML('beforeend', createPhotoCards(data.hits));
+  lightbox.refresh();
+}
---
+++ src/js/ApiPixabay.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export class ApiService {
+  #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '31174976-e4ea20c1e3f3139c1b6ab1378';
+
+  constructor() {
+    this.page = null;
+    this.per_page = 40;
+    this.searchQuery = null;
+  }
+
+  fetchPhotos() {
+    const searchParams = {
+      params: {
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: this.page,
+        key: this.#API_KEY,
+      },
+    };
+    return axios.get(`${this.#BASE_URL}`, searchParams);
+  }
+}
